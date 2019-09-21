c82dee849f5e2129493db726978fbf8dee75323b
helenzzq <helenzhu0411@gmail.com>
2019-09-21 14:33:24 -0400

    Adjust layout and onclick events

--- UofT_map/app/src/main/java/com/example/uoft_map/LocsController.java
+++ UofT_map/app/src/main/java/com/example/uoft_map/LocsController.java
@@ -21,13 +21,13 @@ public class LocsController {
 
     private void setUpInfo(){
         locsManager.addLoc(43.6596,-79.3977,"Bahen Center", "BA",
-                "40 St George St, Toronto, ON M5S 2E4\n");
+        "40 St George St, Toronto, ON M5S 2E4\n");
         locsManager.addLoc(43.666375,-79.389815,"Brennan Hall", "BR",
                 "81 St. Mary Street, Toronto, ON M5S 1J4\n");
         locsManager.addLoc(43.66447,-79.399456,"Robarts Library", "RB",
                 "130 St George St, Toronto, ON M5S 1A5\n");
     }
-    //
+
 
     public Loc getCurrentLoaction() {
         return currentLoaction;
